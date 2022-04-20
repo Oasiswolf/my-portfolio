9181fa070b8722bd119f7219890f6c75fb476729
Nathan Lamb <usmc_wolf@hotmail.com>
2022-04-20 16:03:54 -0500

    added portfolio container component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment'
 
+import PortFolioContainer from './portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Portfolio for Nathan Lamb</h1>
         <h2>This Class is Awesome!</h2>
+        <h1> <PortFolioContainer /> </h1>
         <div>
          {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class PortFolioContainer extends Component {
+  render() {
+    return (
+      <div>PortFolio Container</div>
+    )
+  }
+}
