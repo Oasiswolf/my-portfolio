fd629edcd1c2ec4a890673c332f2b79423558e85
Nathan Lamb <usmc_wolf@hotmail.com>
2022-04-25 17:01:01 -0500

    added basic Nav Feature

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import moment from 'moment'
+import React, { Component } from "react";
+import moment from "moment";
 
-import PortFolioContainer from './portfolio-items/portfolio-container';
+import PortFolioContainer from "./portfolio-items/portfolio-container";
+import Navigation from "./navigation/nav";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>Portfolio for Nathan Lamb</h1>
-        <h2>This Class is Awesome!</h2>
-        <h1> <PortFolioContainer /> </h1>
-        <div>
-         {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
-      </div> 
-    );
-  }
+	render() {
+		return (
+			<div className="app">
+				<Navigation />
+				<h1>Portfolio for Nathan Lamb</h1>
+				<div>
+					<h3>{moment().format("MMMM Do YYYY, h:mm:ss a")}</h3>
+				</div>
+				<h2>This Class is Awesome!</h2>
+				<h1>
+					{" "}
+					<PortFolioContainer />{" "}
+				</h1>
+			</div>
+		);
+	}
 }
--- src/components/portfolio-items/portfolio-item.js
+++ src/components/portfolio-items/portfolio-item.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function portfolio_item() {
-  return (
-    <div>
-      <h2>Portolio Item</h2>
-    </div>
-  )
+export default function portfolio_item(props) {
+	return (
+		<div>
+			<h3>{props.title}</h3>
+			<h3>{props.url}</h3>
+		</div>
+	);
 }
